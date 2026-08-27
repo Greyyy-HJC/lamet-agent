b1b8b38f9764e814e476f06e7b14b4e4b3fa4720
Greyyy-HJC <hejinchen17@gmail.com>
2026-08-26 23:26:21 -0400

    update kernels with fei's latest version

--- lamet_agent/kernels/z_msbar_da_nlo.py
+++ lamet_agent/kernels/z_msbar_da_nlo.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from .implementation import ZMSbar
+from .implementation import ZMSbar_da
 
 
-def kernel(z_fm: np.ndarray | float, mu: float = 2.0, order: int = 0, Nf: int = 3) -> np.ndarray:
-    return ZMSbar(z_fm, mu=mu, offset=3.5, order=order, Nf=Nf)
+def kernel(z_fm: np.ndarray | float, mu: float = 2.0) -> np.ndarray:
+    return ZMSbar_da(z_fm, mu)
